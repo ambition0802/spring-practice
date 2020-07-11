3fa0ff08df56d79d888d591365e829419fe33c4e
sid <673088860@qq.com>
2020-07-11 17:44:36 +0800

    创建项目

---
+++ src/main/java/com/huang/spring/practice/Application.java
@@ -0,0 +1,16 @@
+package com.huang.spring.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
